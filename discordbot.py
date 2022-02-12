dac6f1de939ca324e574c5d3f1bc59f023dacc77
yl0751726 <86944289+yl0751726@users.noreply.github.com>
2022-02-12 22:18:19 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -35,7 +35,7 @@ async def on_message(message):
             await message.reply(remessage) 
             
      if message.content == ('應該'):
-        msg=有自信點 把應該去掉
+        msg = '有自信點 把應該去掉'
         await client.send_message(msg)
         
 # Bot起動
