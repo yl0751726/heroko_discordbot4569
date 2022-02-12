156de9e46cf8408e0f8f5df5b721736076abfe50
yl0751726 <86944289+yl0751726@users.noreply.github.com>
2022-02-12 21:48:01 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -33,9 +33,6 @@ async def on_message(message):
         if translator.detect(content).lang == SRCLanguage or SRCLanguage == '':
             remessage = translator.translate(content, dest='zh-tw').text
             await message.reply(remessage) 
-     
-     if message == 應該:
-            await message.reply(有自信點 把應該去掉)
             
 # Bot起動
 client.run(TOKEN)
