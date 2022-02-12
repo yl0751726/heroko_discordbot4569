3ae951a84db03a1b8b73fc7073fc1b2a2668bc71
yl0751726 <86944289+yl0751726@users.noreply.github.com>
2022-02-12 23:37:24 +0800

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -4,33 +4,8 @@ from pprint import pprint
 
 TOKEN = os.environ['TOKEN']
 
-client = discord.Client()
-
-# 起動時呼叫
-@client.event
+@bot.event
 async def on_ready():
     print('成功登入')
-
-# 收到訊息時呼叫
-@client.event
-async def on_message(message):
-    # 送信者為Bot時無視
-    if message.author == client.user:
-        return
-        
-    if client.user in message.mentions: # @判定
-        translator = googletrans.Translator()
-        robotName = client.user.name
-        first, space, content = message.clean_content.partition('@'+robotName+' ')
-        
-        if content == '':
-            content = first
-        if translator.detect(content).lang == DSTLanguage:
-            return
-        if translator.detect(content).lang == SRCLanguage or SRCLanguage == '':
-            remessage = translator.translate(content, dest='zh-tw').text
-            await message.reply(remessage) 
-
-        
-# Bot起動
-client.run(TOKEN)
+    
+bot.run(TOKEN)
